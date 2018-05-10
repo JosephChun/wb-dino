f06d5234da0c82fa8dfcfcd6ed576df1f07bf2ba
dinochun <godlovesjoe@naver.com>
2018-05-10 11:34:33 +0900

    initial ms

---
+++ src/main/java/net/codesquad/WbDinoApplication.java
@@ -0,0 +1,12 @@
+package net.codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbDinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbDinoApplication.class, args);
+	}
+}
